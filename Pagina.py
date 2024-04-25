8ef280d2677af576656df63131815650bf74c0e0
AlexandeOliveira12 <lebella321@gmail.com>
2024-04-24 22:44:30 -0300

    AjustesDatabase

--- Pagina.py
+++ Pagina.py
@@ -12,7 +12,7 @@ if Alt == 1:
     mydb = mysql.connector.connect(host=config("Host"), user=config("User"), password=config("Password"), database=config("Database"))
 
     mycursor = mydb.cursor()    
-    sql = f"INSERT INTO Usuarios(Usuario, Email, Senha, Endereco_IP) VALUE (%s, %s, %s, %s) "
+    sql = f"INSERT INTO credenciais_usuarios(Usuario, Email, Senha, Endereco_IP) VALUE (%s, %s, %s, %s) "
     val = (Nome, Email, Senha_Hash, Endereco_IP) 
     mycursor.execute(sql, val)
     mydb.commit()
@@ -30,7 +30,7 @@ elif Alt == 2:
     mydb = mysql.connector.connect(host=config("Host"), user=config("User"), password=config("Password"), database=config("Database"))
 
     mycursor = mydb.cursor()    
-    sql = "UPDATE Usuarios SET Senha = %s WHERE Senha = %s;"
+    sql = "UPDATE credenciais_usuarios SET Senha = %s WHERE Senha = %s;"
     val = (Senha_Update_Hash,Senha_Antiga) 
     mycursor.execute(sql, val)
     mydb.commit()
@@ -42,7 +42,7 @@ elif Alt == 3:
     mydb = mysql.connector.connect(host=config("Host"), user=config("User"), password=config("Password"), database=config("Database"))
 
     mycursor = mydb.cursor()    
-    sql = "UPDATE Usuarios SET Email = %s WHERE Usuario = %s AND Senha = %s;"
+    sql = "UPDATE credenciais_usuarios SET Email = %s WHERE Usuario = %s AND Senha = %s;"
     val = (EmailAtualizado,Usuario_Email, Senha_Hash_Formatada) 
     mycursor.execute(sql, val)
     mydb.commit()
